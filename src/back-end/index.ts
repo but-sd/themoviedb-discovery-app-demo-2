ca3cc881c536cc840d7a5137c63e713150a6dc61
alexandre-girard <alexandre.girard@maif.fr>
2026-09-23 17:53:41 +0200

    fix: wrap query parameter handling in try block for error management

--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -14,20 +14,22 @@ const app = express();
 app.get(
   '/api/movies/popular',
   async (_req: express.Request, res: express.Response) => {
-    // Create a URLSearchParams object to build the query string for the TMDB API request
-    const queryParams = new URLSearchParams();
-
-    // Extract query parameters from the request and append them to the query string
-    const { language, page, region } = _req.query;
-
-    queryParams.append('language', (language as string) || DEFAULT_LANGUAGE);
-    queryParams.append('page', (page as string) || DEFAULT_PAGE);
-    queryParams.append('region', (region as string) || DEFAULT_REGION);
-
-    // log the query parameters for debugging purposes
-    console.log('Query Params:', queryParams.toString());
-
     try {
+      // Create a URLSearchParams object to build the query string for the TMDB API request
+      const queryParams = new URLSearchParams();
+
+      // Extract query parameters from the request and append them to the query string
+      const { language, page, region } = _req.query;
+
+      // Append the query parameters to the URLSearchParams object, using default values if not provided in the request
+      // parameters: language, page, region
+      queryParams.append('language', (language as string) || DEFAULT_LANGUAGE);
+      queryParams.append('page', (page as string) || DEFAULT_PAGE);
+      queryParams.append('region', (region as string) || DEFAULT_REGION);
+
+      // log the query parameters for debugging purposes
+      console.log('Query Params:', queryParams.toString());
+
       const response = await fetch(
         `https://api.themoviedb.org/3/movie/popular?${queryParams.toString()}`,
         {
